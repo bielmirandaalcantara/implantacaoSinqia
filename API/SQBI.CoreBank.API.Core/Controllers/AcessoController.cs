58c2eecc67ff53ccc4f31bee22f691638b8c3c41
Gabriel Alcantara <biel.miranda.alcantara@gmail.com>
2020-09-01 18:24:34 -0300

    Inclusao do controller de acesso

---
+++ API/SQBI.CoreBank.API.Core/Controllers/AcessoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SQBI.CoreBank.API.Core.Controllers
+{
+    [ApiController]
+    public class AcessoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/core/acesso/token")]
+        public IActionResult getToken()
+        {
+            return Ok();
+        }
+    }
+}
