792efd217651c8bc0afcdee5f022e24e123f209f
Gabriel Alcantara <gabriel.alcantara@sinqia.com.br>
2020-09-01 13:04:26 -0300

    Commit estrutura primeira versao

--- API/SQBI.CoreBank.API.Core/Models/MsgPessoa.cs
+++ API/SQBI.CoreBank.API.Core/Models/MsgPessoa.cs
@@ -181,4 +181,116 @@ namespace SQBI.CoreBank.API.Core.Models
         public MsgRegistroendereco[] RegistroEndereco { get; set; }
         public MsgRegistroreferencia[] RegistroReferencia { get; set; }
     }
+
+    public class MsgRegistroperfil
+    {
+        public string codigoPessoa { get; set; }
+        public string codigoPerfil { get; set; }
+    }
+
+    public class MsgRegistrodocumento
+    {
+        public string codigoPessoa { get; set; }
+        public string codigoFilial { get; set; }
+        public float codigoEndereco { get; set; }
+        public string tipoEndereco { get; set; }
+        public string tipoLogradouro { get; set; }
+        public string nomeLogradouro { get; set; }
+        public string complementoLogradouro { get; set; }
+        public string nomeBairro { get; set; }
+        public string Cep { get; set; }
+        public string codigoDddFone1 { get; set; }
+        public string codigoDddFone2 { get; set; }
+        public string codigoDddFone3 { get; set; }
+        public string codigoDddFone4 { get; set; }
+        public string numeroTelefone1 { get; set; }
+        public string numeroTelefone2 { get; set; }
+        public string numeroTelefone3 { get; set; }
+        public string numeroTelefone4 { get; set; }
+        public string numeroRamal1 { get; set; }
+        public string numeroRamal2 { get; set; }
+        public string numeroRamal3 { get; set; }
+        public string numeroRamal4 { get; set; }
+        public string situacaoTelefone1 { get; set; }
+        public string situacaoTelefone2 { get; set; }
+        public string situacaoTelefone3 { get; set; }
+        public string situacaoTelefone4 { get; set; }
+        public string codigoDddFax1 { get; set; }
+        public string codigoDddFax2 { get; set; }
+        public string codigoDddFax3 { get; set; }
+        public string numeroFax1 { get; set; }
+        public string numeroFax2 { get; set; }
+        public string numeroFax3 { get; set; }
+        public string email { get; set; }
+        public string indicadorSituacaoResidencia { get; set; }
+        public string indicadorCorrespondencia { get; set; }
+        public string dataInicial { get; set; }
+        public string dataFinal { get; set; }
+        public string dataCadastro { get; set; }
+        public string usuarioUltimaAtualizacao { get; set; }
+        public string dataAtualizacao { get; set; }
+        public string indicadorSituacao { get; set; }
+        public string dataSituacao { get; set; }
+        public float codigoMunicipio { get; set; }
+        public string descricaoMunicipio { get; set; }
+        public string numeroEndereco { get; set; }
+        public string indicadorEnvioCorrespondencia { get; set; }
+        public string codigoMotivo { get; set; }
+        public string indicadorSituacaoRegistro { get; set; }
+        public string enderecoEstrangeiro { get; set; }
+        public float codigoPais { get; set; }
+        public string codigoDdiFone1 { get; set; }
+        public string codigoDdiFone2 { get; set; }
+        public string codigoDdiFone3 { get; set; }
+        public string codigoDdiFone4 { get; set; }
+        public string descricaoMunicipioInternacional { get; set; }
+        public string descricaoEstadoInternacional { get; set; }
+    }
+
+    public class MsgRegistroendereco
+    {
+        public string codigoPessoa { get; set; }
+        public string numeroDocumento { get; set; }
+        public string dataExpedicao { get; set; }
+        public string orgaoExpedidor { get; set; }
+        public string observacao { get; set; }
+        public string dataCadastro { get; set; }
+        public string usuarioUltimaAtualizacao { get; set; }
+        public string dataAtualizacao { get; set; }
+        public string IndicadorSituacao { get; set; }
+        public string dataSituacao { get; set; }
+        public string tipoDocumento { get; set; }
+        public string ufExpedicao { get; set; }
+        public string documentoCheque { get; set; }
+        public string indicadorMicroEmpresa { get; set; }
+        public string IndicadorComprovado { get; set; }
+        public float tipoComprovacaoRenda { get; set; }
+        public string indicadorPreposto { get; set; }
+        public string dataVencimento { get; set; }
+        public float codigoNacionalidade { get; set; }
+    }
+
+    public class MsgRegistroreferencia
+    {
+        public string codigoPessoaTitular { get; set; }
+        public string codigoFilialTitular { get; set; }
+        public float sequencial { get; set; }
+        public string tipo { get; set; }
+        public string observacao { get; set; }
+        public float numeroCartao { get; set; }
+        public float valorLimite { get; set; }
+        public string dataInicioEmprego { get; set; }
+        public string dataFinalEmprego { get; set; }
+        public string dataCadastro { get; set; }
+        public string usuarioUltimaAtualizacao { get; set; }
+        public string dataAtualizacao { get; set; }
+        public string indicadorSituacao { get; set; }
+        public string dataSituacao { get; set; }
+        public float codigoCartao { get; set; }
+        public float codigoSeguradora { get; set; }
+        public string codigoPessoaReferencia { get; set; }
+        public string codigoFilialReferencia { get; set; }
+        public string codigoPessoaSimplificada { get; set; }
+        public string dataVencimentoSeguroCartao { get; set; }
+    }
 }
