5fc2fa84ba65c711508a5a01d1165951d8de725f
Gabriel Alcantara <biel.miranda.alcantara@gmail.com>
2020-09-09 15:34:13 -0300

    inclusao das classes de serviço para o envio do XML

---
+++ API/Sinqia.CoreBank.Services.CUC/Services/Autenticacao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+using Sinqia.CoreBank.Services.CUC.Autenticacao;
+
+namespace Sinqia.CoreBank.Services.CUC.Services
+{
+    public class Autenticacao
+    {
+        CucCliAutenticacaoClient _ServiceClient;
+        public CucCliAutenticacaoClient ServiceClient { get
+            {
+                if (_ServiceClient == null) _ServiceClient = new CucCliAutenticacaoClient();
+                return _ServiceClient;
+            }
+        }
+
+    
+        public string GetToken(string login, string senha)
+        {
+            string token = string.Empty;
+
+            try
+            {
+                var retorno = ServiceClient.AutenticarAsync(login, senha);
+                CucCluRetornoAutenticacao dadosRetorno = retorno.Result;
+                token = dadosRetorno.Token;
+
+                return token;
+            }
+            catch (TimeoutException timeout)
+            {
+                ServiceClient.Abort();
+                throw;
+            }
+            catch (CommunicationException commException)
+            {
+                ServiceClient.Abort();
+                throw;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
