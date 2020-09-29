13c8b1aaf089b6726158b537442fbe009084408f
Diogo Amaral <diogo.amaral@sinqia.com.br>
2020-09-29 15:52:00 -0300

    Alteração grupo empresarial

---
+++ API/Sinqia.CoreBank.API.Core/Controllers/Corporativo/GrupoEmpresarialController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sinqia.CoreBank.API.Core.Controllers.Corporativo
+{
+    public class GrupoEmpresarialController
+    {
+    }
+}
