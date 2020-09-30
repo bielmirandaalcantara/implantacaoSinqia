5d0870ae27172d590e0c17fe8a831f24f8c26fc9
Gabriel Alcantara <biel.miranda.alcantara@gmail.com>
2020-09-30 10:37:17 -0300

    Ajuste no documento de instalação

---
+++ InfraEstrutura/Sinqia.CoreBank.Persistencia/Constantes/ConstantesDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sinqia.CoreBank.Persistencia.Constantes
+{
+    public static class ConstantesDatabase
+    {
+        public const string CAMINHODB = "CAMINHODB";
+    }
+}
