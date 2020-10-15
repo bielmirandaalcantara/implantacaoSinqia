f5a9709ae5d78215384c5ff79a285ae57d180e0d
Diogo Amaral <diogo.amaral@sinqia.com.br>
2020-10-15 14:44:51 -0300

    Inserido validações nos services de operador de pendencia e dependencia

--- BLL/Sinqia.CoreBank.BLL.Corporativo/Services/tb_dependenciaService.cs
+++ BLL/Sinqia.CoreBank.BLL.Corporativo/Services/tb_dependenciaService.cs
@@ -32,6 +32,17 @@ namespace Sinqia.CoreBank.BLL.Corporativo.Services
 
             tb_dependencia retorno = null;
 
+            if (cod_empresa == null || cod_empresa <= 0)
+                throw new ApplicationException("Código da empresa inválido");
+
+            if (cod_depend == null || cod_depend <= 0)
+                throw new ApplicationException("Código de dependencia inválido");
+
+            tb_empresa empresa = _empresaService.BuscarEmpresaPorCodigo(cod_empresa, transacao);
+
+            if (empresa == null)
+                throw new ApplicationException("Empresa informada não cadastrada");
+
             string where = $" cod_empresa = {cod_empresa} and cod_depend = {cod_depend} ";
 
             var listaEntity = dao.Obter(where);
@@ -43,8 +54,20 @@ namespace Sinqia.CoreBank.BLL.Corporativo.Services
 
         }
 
-        public IEnumerable<tb_dependencia> BuscarDependencias(int cod_empresa, int cod_depend)
+        public IEnumerable<tb_dependencia> BuscarDependencias(int cod_empresa, int cod_depend, IDaoTransacao transacao = null)
         {
+            
+            if (cod_empresa == null || cod_empresa <= 0)
+                throw new ApplicationException("Código da empresa inválido");
+
+            if (cod_depend == null || cod_depend <= 0)
+                throw new ApplicationException("Código de dependencia inválido");
+
+            tb_empresa empresa = _empresaService.BuscarEmpresaPorCodigo(cod_empresa, transacao);
+
+            if (empresa == null)
+                throw new ApplicationException("Empresa informada não cadastrada");
+
             var dao = _factory.GetDaoCorporativo<tb_dependencia>();
 
             string where = $" cod_empresa = {cod_empresa} and cod_depend = {cod_depend} ";
@@ -105,6 +128,42 @@ namespace Sinqia.CoreBank.BLL.Corporativo.Services
         {
             var dao = transacao == null ? _factory.GetDaoCorporativo<tb_dependencia>() : _factory.GetDaoCorporativo<tb_dependencia>(transacao);
 
+            if (entity.cod_empresa == null || entity.cod_empresa.Value <= 0)
+                throw new ApplicationException("Código da empresa inválido");
+
+            tb_empresa empresa = _empresaService.BuscarEmpresaPorCodigo(entity.cod_empresa.Value, transacao);
+            if (empresa == null)
+                throw new ApplicationException("Empresa informada não cadastrada");
+
+            if (entity.cod_municipio != null)
+            {
+                var daoMunicipio = _factory.GetDaoCorporativo<tb_municipio>(transacao);
+                var municipio = daoMunicipio.ObterPrimeiro($" cod_municipio = {entity.cod_municipio} ");
+                if (municipio == null)
+                    throw new ApplicationException("Município não encontrado na base de dados");
+            }
+            else
+                throw new ApplicationException("Município obrigatório");
+
+            if (entity.tip_tpdepend != null)
+            {
+                var daoTpDepend = _factory.GetDaoCorporativo<tb_tpdepend>(transacao);
+                var tipoDepend = daoTpDepend.ObterPrimeiro($" tip_tpdepend  = '{entity.tip_tpdepend}' ");
+                if (tipoDepend == null)
+                    throw new ApplicationException("Tipo de dependência inválido");
+            }
+            else
+                throw new ApplicationException("Tipo de dependência obrigatório");
+
+            if (entity.idc_sit != null)
+            {
+                if (!entity.idc_sit.ToUpper().Equals(tb_dependencia.situacaoAtivo)
+                  && !entity.idc_sit.ToUpper().Equals(tb_dependencia.situacaoInativo))
+                    throw new ApplicationException("Campo indicador de situação inválido");
+            }
+            else
+                throw new ApplicationException("Campo indicador de situação obrigatório");
+
             string where = $" cod_empresa = {entity.cod_empresa} and cod_depend = {entity.cod_depend} ";
 
             var entityBanco = dao.Obter(where);
@@ -119,6 +178,17 @@ namespace Sinqia.CoreBank.BLL.Corporativo.Services
         public void ExcluirDependencia(int cod_empresa, int cod_depend, IDaoTransacao transacao = null)
         {
             var dao = transacao == null ? _factory.GetDaoCorporativo<tb_dependencia>() : _factory.GetDaoCorporativo<tb_dependencia>(transacao);
+
+            if (cod_empresa == null || cod_empresa <= 0)
+                throw new ApplicationException("Código da empresa inválido");
+
+            if (cod_depend == null || cod_depend <= 0)
+                throw new ApplicationException("Código de dependencia inválido");
+
+            tb_empresa empresa = _empresaService.BuscarEmpresaPorCodigo(cod_empresa, transacao);
+
+            if (empresa == null)
+                throw new ApplicationException("Empresa informada não cadastrada");
 
             string where = $" cod_empresa = {cod_empresa} and cod_depend = {cod_depend} ";
 
