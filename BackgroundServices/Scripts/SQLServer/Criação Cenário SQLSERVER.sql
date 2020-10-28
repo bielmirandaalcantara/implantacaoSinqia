d0663fcd3e54ecf3358d41c7c256364f686c70d4
Diogo Amaral <diogo.amaral@sinqia.com.br>
2020-10-28 17:15:01 -0300

    Alterado script sql server

--- BackgroundServices/Scripts/SQLServer/Criação Cenário SQLSERVER.sql
+++ BackgroundServices/Scripts/SQLServer/Criação Cenário SQLSERVER.sql
@@ -21,3 +21,16 @@ create table tb_depusrSINCRO(
 	, SSTATUSINTEGRACAO varchar(10) not null
 	, SDATAINTEGRACAO datetime not null
 )
+
+
+create trigger [dbo].[tg_tb_depusr_ins_sincro] ON [dbo].[tb_depusr] after insert as
+begin
+insert into tb_depusrSINCRO (emp_cod,depend_cod,usuar_cod, SCHAVEINTEGRACAO, SMETODO, SQTDETENTATIVA, SSTATUSINTEGRACAO, SDATAINTEGRACAO)
+select emp_cod,depend_cod,usuar_cod, null,'INSERT',0, 'NEW' ,GETDATE() from inserted
+end
+
+create trigger [dbo].[tg_tb_depusr_del_sincro] ON [dbo].[tb_depusr] after delete as
+begin
+insert into tb_depusrSINCRO (emp_cod,depend_cod,usuar_cod, SCHAVEINTEGRACAO, SMETODO, SQTDETENTATIVA, SSTATUSINTEGRACAO, SDATAINTEGRACAO)
+select emp_cod,depend_cod,usuar_cod, null,'DELETE',0, 'NEW' ,GETDATE() from deleted
+end
