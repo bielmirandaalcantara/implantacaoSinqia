6834b2152850340fd92a595451a7c82e0f979c00
Diogo Amaral <diogo.amaral@sinqia.com.br>
2020-11-11 15:46:07 -0300

    Inserido variavel de controle de versão

---
+++ API/Sinqia.CoreBank.API.Core/Controllers/VerificarVersaoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sinqia.CoreBank.API.Core.Models.Corporativo.Templates;
+using Sinqia.CoreBank.ConstantesGerais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sinqia.CoreBank.API.Core.Controllers
+{
+    public class VerificarVersaoController : ControllerBase
+    {
+        /// <summary>
+        /// Busca a Versão da API
+        /// </summary>
+        /// <returns>MsgRetorno</returns>
+        [HttpGet]
+        [Route("api/core/verificarversao")]
+        [ProducesResponseType(typeof(MsgOperadorTemplate), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(MsgOperadorTemplate), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(MsgOperadorTemplate), StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult getVersao()
+        {
+            try
+            {
+                return StatusCode((int)HttpStatusCode.OK, ControleVersao.VersaoAPI + "/" + ControleVersao.VersaoCUC);
+            }
+            catch (ApplicationException appEx)
+            {
+                return StatusCode((int)HttpStatusCode.BadRequest, "Não foi possível verificar a versão");
+            }
+        }
+    }
+}
---
+++ Sinqia.CoreBank.ConstantesGerais/ControleVersao.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Sinqia.CoreBank.ConstantesGerais
+{
+    public class ControleVersao
+    {
+        public const string VersaoAPI = "v1.3.0.0";
+        public const string VersaoCUC = "v06.040.000";
+    }
+}
